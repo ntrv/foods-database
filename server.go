969a3f25faab06d1db7ba4eca9786738070bd75a
ntrv <hydrangea56@me.com>
2018-07-15 04:57:12 +0900

    Add routing

--- server.go
+++ server.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"net/http"
-	
 	"github.com/labstack/echo"
+	"github.com/ntrv/foods-database/handler"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", handler.AllFoods())
 	e.Logger.Fatal(e.Start(":1323"))
 }
