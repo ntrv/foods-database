f96629ea44ca260226af1721dc1c784eb4e4bc7f
ntrv <hydrangea56@me.com>
2018-07-15 05:21:40 +0900

    Use middleware

--- server.go
+++ server.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/ntrv/foods-database/handler"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+
 	e.GET("/", handler.AllFoods())
-	e.Logger.Fatal(e.Start(":1323"))
+
+	s := &http.Server{
+		Addr:         ":1323",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	e.Logger.Fatal(e.StartServer(s))
 }
